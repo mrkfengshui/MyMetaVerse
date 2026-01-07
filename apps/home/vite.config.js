83d4df25b559ac71ca6084ac292dd894255f1b56
mrkfengshui <masterk@mrkfengshui.com>
2026-01-07 21:41:31 +0800

    建構home

--- apps/home/vite.config.js
+++ apps/home/vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path' // <--- 1. 引入 path
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      // 2. 強制指定：當有人叫 @my-meta/ui 時，直接去隔壁房找 index.jsx
+      '@my-meta/ui': path.resolve(__dirname, '../../packages/ui'),
+    },
+  },
+  server: {
+    port: 3004,
+  },
 })
