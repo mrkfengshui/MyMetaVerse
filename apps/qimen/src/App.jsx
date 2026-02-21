700c5dc187b090748368c212c90c834e3c66217c
mrkfengshui <masterk@mrkfengshui.com>
2026-02-21 09:45:58 +0800

    app name

--- apps/qimen/src/App.jsx
+++ apps/qimen/src/App.jsx
@@ -27,7 +27,7 @@ import {
 // =========================================================================
 // PART A: 核心數據與邏輯
 // =========================================================================
-const APP_NAME = "甯博奇門遁甲";
+const APP_NAME = "甯博陰盤奇門遁甲";
 const APP_VERSION = "v1.3 增加宮位資訊";
 const API_URL = "https://script.google.com/macros/s/AKfycbzZRwy-JRkfpvrUegR_hpETc3Z_u5Ke9hpzSkraNSCEUCLa7qBk636WOCpYV0sG9d1h/exec";
 
