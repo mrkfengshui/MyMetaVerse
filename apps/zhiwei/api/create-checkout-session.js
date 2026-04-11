386ce5538306719b7cb10ed07c736e2c8adf6971
mrkfengshui <masterk@mrkfengshui.com>
2026-04-11 14:52:12 +0800

    bazi stripe

--- apps/zhiwei/api/create-checkout-session.js
+++ apps/zhiwei/api/create-checkout-session.js
@@ -1,1 +1,7 @@
-export { default } from '../../../api/create-checkout-session.js';
+// 使用 require 引入最外層的處理器
+const checkoutHandler = require('../../../api/create-checkout-session.js');
+
+// 轉發請求
+module.exports = async function handler(req, res) {
+  return checkoutHandler(req, res);
+};
